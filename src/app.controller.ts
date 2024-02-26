290bab921469426c90fd0715d82c5f4f8f1cf96f
luminary <luminaryhero80@gmail.com>
2024-02-26 20:09:46 +0800

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/api/user')
+  getUser(@Query('name') name: string) {
+    return {
+      code: 0,
+      message: 'ok',
+      data: {
+        name: name,
+      },
+    };
+  }
+}
